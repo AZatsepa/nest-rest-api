94d27a24459ac5fb2cd10797852d4f6c57730372
Artem Zatsepa <Artem_Zatsepa@epam.com>
2021-10-22 09:29:08 +0300

    Add MongoDB

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,6 +7,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Response, Request } from 'express';
 import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema'
 
 @Controller('products')
 export class ProductsController {
@@ -22,29 +23,29 @@ export class ProductsController {
   // }
 
   @Get()
-  getAll() {
+  getAll(): Promise<Product[]> {
     return this.productsService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string) {
+  getOne(@Param('id') id: string): Promise<Product> {
     return this.productsService.getById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productsService.create(createProductDto);
   }
 
   @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string): string {
-  return `Updated by ID: ${id} - Title: ${updateProductDto.title}, Price: ${updateProductDto.price}`;
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string): Promise<Product> {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): string {
-    return 'Deleted: ' + id;
+  remove(@Param('id') id: string): Promise<Product> {
+    return this.productsService.remove(id);
   }
 }
